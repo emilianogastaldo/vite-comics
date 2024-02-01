eac2729aaf06f79121da8e9026259a7c003fe275
emilianogastaldo <emiliano0303@gmail.com>
2024-02-01 17:44:59 +0100

    added footer-top and bottom

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,13 +6,42 @@ export default {
 
 <template>
     <footer>
-        prova footer
+
+        <section id="footer-top" class="wrapper">
+
+
+        </section>
+        <section id="footer-bottom">
+            <div class="wrapper"></div>
+        </section>
+
     </footer>
 </template>
 
 <style scoped>
 footer {
-    background-color: black;
+
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
     color: white
+}
+
+#footer-top {
+    background-image: url('../assets/img/dc-logo-bg.png');
+    background-repeat: no-repeat;
+    background-position: right;
+    padding: 2rem 0;
+}
+
+#footer-bottom {
+    background-color: #303030;
+    padding: 2rem 0;
+}
+
+.wrapper {
+    width: 1200px;
+    margin: 0 auto;
 }
 </style>
