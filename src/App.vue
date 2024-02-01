c52afd51df7edf842fecdfef0b231753c35932e3
emilianogastaldo <emiliano0303@gmail.com>
2024-02-01 15:58:31 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
     name: 'App',
     components: {
         AppHeader,
         AppMain,
+        AppFooter,
     }
 }
 </script>
@@ -14,7 +17,7 @@ export default {
 <template>
     <AppHeader />
     <AppMain />
-    <h1>prova</h1>
+    <AppFooter />
 </template>
 
 <style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <footer>
+        prova footer
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: black;
+    color: white
+}
+</style>
