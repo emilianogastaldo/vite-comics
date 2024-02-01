02bf634f5ab1b662c59ea98e3673df28d2bbb86b
emilianogastaldo <emiliano0303@gmail.com>
2024-02-01 17:03:41 +0100

    added main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,12 +6,27 @@ export default {
 
 <template>
     <main>
-        prova main
+        <section class="wrapper">
+            <p>
+                CONTENT GOES HERE
+            </p>
+        </section>
     </main>
 </template>
 
 <style scoped>
 main {
-    background-color: blueviolet;
+    background-color: #1C1C1C;
+    color: white;
+    height: 100px;
+}
+
+section {
+    line-height: 100px;
+}
+
+.wrapper {
+    width: 1200px;
+    margin: 0 auto;
 }
 </style>
