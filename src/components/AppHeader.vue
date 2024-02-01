5e562ac08d0a6fc65c944ff73b55ad381d672aac
emilianogastaldo <emiliano0303@gmail.com>
2024-02-01 16:31:14 +0100

    added logo and link list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,99 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data: () => ({
+        links: [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+        ]
+    })
 }
 </script>
 
-<template>
+<template >
     <header>
-        prova header
+        <div class="container flex">
+            <figure>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </figure>
+            <nav>
+                <ul class="flex">
+                    <li v-for="link in links"><a :href="link.url">{{ link.text }}</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
 <style scoped>
 header {
-    background-color: bisque;
+    background-color: #FFFFFF;
+    margin-top: 1rem;
+    padding: 1rem 0;
+
+}
+
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul li a {
+
+    padding: 1rem
 }
 </style>
