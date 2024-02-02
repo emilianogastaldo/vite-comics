2a6e3aff087530e88cd9918812b0d5338f02bc59
emilianogastaldo <emiliano0303@gmail.com>
2024-02-02 02:35:04 +0100

    prova

--- src/components/AppBlueSection.vue
+++ src/components/AppBlueSection.vue
@@ -35,27 +35,21 @@ export default {
 
 <template>
     <section>
-        <<<<<<< HEAD <ul class="wrapper">
-
-            LISTA
-            =======
-            <ul class="wrapper flex space-around">
-                <li v-for="item in buyList">
-                    <a href="#">
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        {{ item.text }}
-                    </a>
-                </li>
-                >>>>>>> AppBlueSection
-            </ul>
+        <ul class="wrapper flex space-around">
+            <li v-for="item in buyList">
+                <a href="#">
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    {{ item.text }}
+                </a>
+            </li>
+        </ul>
     </section>
 </template>
 <style lang="scss" scoped>
 section {
     background-color: #0282F9;
-    <<<<<<< HEAD color: white;
-    padding: 10px 0;
-    =======padding: 3rem 0;
+    color: white;
+    padding: 3rem 0;
 
 }
 
@@ -68,6 +62,5 @@ a {
     display: flex;
     align-items: center;
     gap: 1rem;
-    >>>>>>>AppBlueSection
 }
 </style>
