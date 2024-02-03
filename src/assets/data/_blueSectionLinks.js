9b4d2699049cc7193e1aea3a768cb160fd3feb0d
emilianogastaldo <emiliano0303@gmail.com>
2024-02-03 15:41:27 +0100

    create partitial file for blue sesction

---
+++ src/assets/data/_blueSectionLinks.js
@@ -0,0 +1,7 @@
+export default [
+    { text: 'digital comics', img: 'buy-comics-digital-comics.png', url: '#' },
+    { text: 'dc merchandise', img: 'buy-comics-merchandise.png', url: '#' },
+    { text: 'subscription', img: 'buy-comics-subscriptions.png', url: '#' },
+    { text: 'comic shop locator', img: 'buy-comics-shop-locator.png', url: '#' },
+    { text: 'dc power visa', img: 'buy-dc-power-visa.svg', url: '#' },
+]
