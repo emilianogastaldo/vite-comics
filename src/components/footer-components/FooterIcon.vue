9c5f65630fe6642db176568bd7f01c3ce1a3a04b
emilianogastaldo <emiliano0303@gmail.com>
2024-02-03 15:28:08 +0100

    created component for footer icon

---
+++ src/components/footer-components/FooterIcon.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'FooterIcon',
+    props: {
+        icon: Object
+    },
+
+    computed: {
+        iconPath() {
+            return new URL(`../../assets/img/footer_images/${this.icon.img}`, import.meta.url).href
+        }
+    }
+
+}
+</script>
+
+<template>
+    <!-- <a href="#"><img :src="this.img" alt="#"></a> -->
+    <a :href="icon.href"><img :src="this.iconPath" :alt="icon.name"></a>
+</template>
+
+<style lang="scss" scoped>
+img:hover {
+    filter: brightness(200%);
+}
+</style>
