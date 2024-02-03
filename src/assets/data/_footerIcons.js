5250933f7ba4f101c6cbc39258eef8b2a57b6ec8
emilianogastaldo <emiliano0303@gmail.com>
2024-02-03 12:17:26 +0100

    added footer icons data

---
+++ src/assets/data/_footerIcons.js
@@ -0,0 +1,7 @@
+export default [
+    { name: 'Facebook icon', img: 'footer-facebook.png', href: "#" },
+    { name: 'Twitter icon', img: 'footer-twitter.png', href: "#" },
+    { name: 'Youtube icon', img: 'footer-youtube.png', href: "#" },
+    { name: 'Pinterest icon', img: 'footer-pinterest.png', href: "#" },
+    { name: 'Periscope icon', img: 'footer-periscope.png', href: "#" }
+]
--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,4 +1,5 @@
 // Barrell dei dati
 import footerMenus from './_footerMenus';
+import footerIcons from './_footerIcons';
 
-export { footerMenus };
+export { footerMenus, footerIcons };
